8e3754686f95425625ce86df8ff55719e90638b4
huunvx <huunvx@gmail.com>
2016-10-13 13:05:25 +0700

    Preview Login Activity and SignUp Activity
    Preview

--- app/src/main/java/net/phuanh004/eznote/SignUpActivity.java
+++ app/src/main/java/net/phuanh004/eznote/SignUpActivity.java
@@ -1,13 +1,55 @@
 package net.phuanh004.eznote;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class SignUpActivity extends AppCompatActivity {
-
+    EditText etEmails,etPasss,etNames,etPhones;
+    Button btnSignUp;
+    private FirebaseAuth mAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+        mAuth = FirebaseAuth.getInstance();
+        etEmails = (EditText)findViewById(R.id.etEmails);
+        etPasss = (EditText)findViewById(R.id.etPasss);
+        etNames = (EditText)findViewById(R.id.etNames);
+        etPhones = (EditText)findViewById(R.id.etPhones);
+        btnSignUp = (Button)findViewById(R.id.btnSignUp);
+
+
+
+    }
+
+    private void SignUp(){
+        String User = etEmails.getText().toString();
+        String Pass = etPasss.getText().toString();
+        mAuth.createUserWithEmailAndPassword(User, Pass)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Intent intent = new Intent(SignUpActivity.this,NotesActivity.class);
+                            startActivity(intent);
+                        }else {
+                            Toast.makeText(SignUpActivity.this, "Failed!",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+
+                        // ...
+                    }
+                });
+
     }
 }
