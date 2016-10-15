61324b31ad9b51454159a015fd9388c14f54a94d
huunvx <huunvx@gmail.com>
2016-10-15 22:58:19 +0700

    fix keyboard edittext

--- app/src/main/java/net/phuanh004/eznote/LoginActivity.java
+++ app/src/main/java/net/phuanh004/eznote/LoginActivity.java
@@ -2,6 +2,7 @@ package net.phuanh004.eznote;
 
 import android.content.Intent;
 import android.support.annotation.NonNull;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -43,9 +44,29 @@ public class LoginActivity extends AppCompatActivity {
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Login();
+                String User = etEmail.getText().toString();
+                String Pass = etPass.getText().toString();
+                if(Pass.equals("") && User.equals("")){
+                    Toast.makeText(LoginActivity.this, "Enter email address and password",
+                            Toast.LENGTH_SHORT).show();
+
+                }
+                else if(Pass.equals("")){
+                    Toast.makeText(LoginActivity.this, "Invalid password",
+                            Toast.LENGTH_SHORT).show();
+
+                }
+                else if(User.equals("")){
+                    Toast.makeText(LoginActivity.this, "Invalid email address",
+                            Toast.LENGTH_SHORT).show();
+                }
+                else{
+                    Login();
+                }
             }
         });
+
+
 
         tvSignUp.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -68,7 +89,7 @@ public class LoginActivity extends AppCompatActivity {
                             Intent intent = new Intent(LoginActivity.this,NotesActivity.class);
                             startActivity(intent);
                         }else {
-                            Toast.makeText(LoginActivity.this, "Failed!",
+                            Toast.makeText(LoginActivity.this, "Incorrect email or password",
                                     Toast.LENGTH_SHORT).show();
                         }
 
